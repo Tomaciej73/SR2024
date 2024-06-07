54f9df097ce589d450715c8a4e5779e360912a22
Tomaciej73 <92037535+Tomaciej73@users.noreply.github.com>
2024-06-07 20:48:54 +0200

    config.py

--- skrypt_api_db/config.py
+++ skrypt_api_db/config.py
@@ -1,22 +1,22 @@
 credentials = {
     "magento_db": {
-        "host": "192.168.1.146",
+        "host": "192.1.1.1",
         "user": "inf2s_163105",
-        "password": "Wiocha22!!",
+        "password": "xxx",
         "database": "magento",
         "port": 3306,
     },
     "presta_db": {
-        "host": "192.168.1.168",
+        "host": "192.1.1.1",
         "user": "inf2s_163105",
-        "password": "Wiocha22!!",
+        "password": "xxx",
         "database": "prestashop",
         "port": 3306,
     },
     "sync_db": {
-        "host": "192.168.1.168",
+        "host": "192.1.1.1",
         "user": "inf2s_163105",
-        "password": "Wiocha22!!",
+        "password": "xxx",
         "database": "zad2",
         "port": 3306,
     },
